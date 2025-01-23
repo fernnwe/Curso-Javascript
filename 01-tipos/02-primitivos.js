a994dea4b727ac336c2c2ed5bf9cd7c91b4dda8c
Fernando Aguirre <yassir468124@gmail.com>
2025-01-22 20:27:05 -0600

    Primer commit del curso de python

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,6 @@
+let numero = 1;
+let texto = "Hola mundo";
+let verdadero = true;
+let falso = false;
+let unDef = undefined;
+let nulo = null;
---
+++ app.js
@@ -0,0 +1,2 @@
+//Aqui obtenemos una impresion en pantalla que nos muestra: "Hola mundo"
+console.log('Hola mundo')
